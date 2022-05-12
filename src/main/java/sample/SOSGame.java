5ed80543b3cf3cac7a614b63ee7f55b900c4ed7e
cpprian <cpprian456@gmail.com>
2022-05-12 21:19:42 +0200

    connect fxml to game stage

--- src/main/java/sample/SOSGame.java
+++ src/main/java/sample/SOSGame.java
@@ -1,12 +1,20 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class SOSGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // TODO: add stage
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
+        primaryStage.setTitle("SOS game");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {

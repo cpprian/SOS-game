14da5ec6a8fce01b2b3b4b7b581bc92e53d6e304
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2022-05-25 01:12:01 +0200

    poprawka funkcji wyszukującej napis SOS

--- src/main/java/sample/GameRules.java
+++ src/main/java/sample/GameRules.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,11 +19,12 @@ public class GameRules {
     private int posx;
     private int posy;
 
+
     public GameRules(ArrayList<Button> arButtons, Button reset, Label playerTurnInfo) {
         buttons = new Button[6][6];
         int button = 0;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 6; j++){
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 buttons[i][j] = arButtons.get(button);
                 button++;
             }
@@ -33,82 +35,91 @@ public class GameRules {
         turn = 1;
     }
 
-    public int searchForNewSOS(Button button){
+    public int searchForNewSOS(Button button) {
         int score = 0;
         setPositions(button);
-        System.out.println(buttons[posx][posy].getText());
-        if(Objects.equals(buttons[posx][posy].getText(), "S")){
-            if(posx > 1 && posy > 1 && Objects.equals(buttons[posx - 1][posy - 1].getText(), "O")){
-                if(Objects.equals(buttons[posx - 1][posy - 1].getText(), "S")){
+        //Oś Y jest poziomo a oś X pionowo
+        score = findSOSVertical(score);
+        score = findSOSHorizontal(score);
+        score = findSOSBias(score);
+        return score;
+    }
+
+    private int findSOSBias(int score) {
+        if (Objects.equals(buttons[posx][posy].getText(), "S")) {
+            // na ukos / sprawdza od lewej w górę
+            if (posx > 1 && posy < 4 && Objects.equals(buttons[posx - 1][posy + 1].getText(), "O")) {
+                if (Objects.equals(buttons[posx - 2][posy + 2].getText(), "S"))
                     score++;
-                }
-            }
-            if(posx > 1 && Objects.equals(buttons[posx - 1][posy].getText(), "O")){
-                if(Objects.equals(buttons[posx - 2][posy].getText(), "S")){
+            }//na ukos / spradza od prawej w dół
+            else if (posx < 4 && posy > 1 && Objects.equals(buttons[posx + 1][posy - 1].getText(), "O")) {
+                if (Objects.equals(buttons[posx + 2][posy - 2].getText(), "S"))
                     score++;
-                }
             }
-            if(posx > 1 && posy < 4 && Objects.equals(buttons[posx - 1][posy + 1].getText(), "O")){
-                if(Objects.equals(buttons[posx - 2][posy + 2].getText(), "S")){
+            // na ukos \ sprawdza od lewej w dół
+            if (posx < 4 && posy < 4 && Objects.equals(buttons[posx + 1][posy + 1].getText(), "O")) {
+                if (Objects.equals(buttons[posx + 2][posy + 2].getText(), "S"))
                     score++;
-                }
-            }
-            if(posy > 1 && Objects.equals(buttons[posx][posy - 1].getText(), "O")){
-                if(Objects.equals(buttons[posx][posy - 2].getText(), "S")){
+            }//na ukos \ spradza od prawej w górę
+            else if (posx > 1 && posy > 1 && Objects.equals(buttons[posx - 1][posy - 1].getText(), "O")) {
+                if (Objects.equals(buttons[posx - 2][posy - 2].getText(), "S"))
                     score++;
-                }
             }
-            if(posy < 4 && Objects.equals(buttons[posx][posy + 1].getText(), "O")){
-                if(Objects.equals(buttons[posx][posy + 2].getText(), "S")){
-                    score++;
-                }
-            }
-            if(posx < 4 && posy > 1 && Objects.equals(buttons[posx + 1][posy - 1].getText(), "O")){
-                if(Objects.equals(buttons[posx + 1][posy - 1].getText(), "S")){
-                    score++;
-                }
-            }
-            if(posx < 4 && Objects.equals(buttons[posx + 1][posy].getText(), "O")){
-                if(Objects.equals(buttons[posx + 2][posy].getText(), "S")){
-                    score++;
-                }
-            }
-            if(posx < 4 && posy < 4 && Objects.equals(buttons[posx + 1][posy + 1].getText(), "O")){
-                if(Objects.equals(buttons[posx + 2][posy + 2].getText(), "S")){
-                    score++;
-                }
-            }
-        }
-        else{
-            //pionowe
-            if(posx > 0 && posx < 5 && Objects.equals(buttons[posx - 1][posy].getText(), "S")){
-                if(Objects.equals(buttons[posx +1][posy].getText(), "S")){
-                    score++;
-                }
-            }
-            //poziome
-            if(posy > 0 && posy < 5 && Objects.equals(buttons[posx][posy - 1].getText(), "S")){
-                if(Objects.equals(buttons[posx][posy + 1].getText(), "S")){
-                    score++;
-                }
-            }
-            if(posx > 0 && posy > 0 && posx < 5 && posy < 5){
+        } else {
+            if (posx > 0 && posy > 0 && posx < 5 && posy < 5) {
                 //skos \
-                if(Objects.equals(buttons[posx - 1][posy - 1].getText(), "S") && Objects.equals(buttons[posx + 1][posy + 1].getText(), "S")){
+                if (Objects.equals(buttons[posx - 1][posy - 1].getText(), "S") && Objects.equals(buttons[posx + 1][posy + 1].getText(), "S")) {
                     score++;
                 }
                 // skos /
-                if(Objects.equals(buttons[posx + 1][posy - 1].getText(), "O") && Objects.equals(buttons[posx - 1][posy + 1].getText(), "S")){
+                if (Objects.equals(buttons[posx + 1][posy - 1].getText(), "S") && Objects.equals(buttons[posx - 1][posy + 1].getText(), "S")) {
                     score++;
                 }
             }
         }
         return score;
     }
-    private void setPositions(Button button){
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 6; j++){
-                if(button == buttons[i][j]){
+
+    private int findSOSHorizontal(int score) {
+        if (Objects.equals(buttons[posx][posy].getText(), "S")) {
+            if (posy > 1 && Objects.equals(buttons[posx][posy - 1].getText(), "O")) {
+                if (Objects.equals(buttons[posx][posy - 2].getText(), "S"))
+                    score++;
+            } else if (posy < 4 && Objects.equals(buttons[posx][posy + 1].getText(), "O")) {
+                if (Objects.equals(buttons[posx][posy + 2].getText(), "S"))
+                    score++;
+            }
+        } else {
+            if (posx > 0 && posx < 5 && Objects.equals(buttons[posx - 1][posy].getText(), "S"))
+                if (Objects.equals(buttons[posx + 1][posy].getText(), "S"))
+                    score++;
+        }
+        return score;
+    }
+
+    private int findSOSVertical(int score) {
+        if (Objects.equals(buttons[posx][posy].getText(), "S")) {
+            if (posx > 1 && Objects.equals(buttons[posx - 1][posy].getText(), "O")) {
+                if (Objects.equals(buttons[posx - 2][posy].getText(), "S"))
+                    score++;
+            } else if (posx <4) {
+                if (Objects.equals(buttons[posx + 1][posy].getText(), "O"))
+                    if (Objects.equals(buttons[posx + 2][posy].getText(), "S"))
+                        score++;
+            }
+
+        } else {
+            if (posy > 0 && posy < 5 && Objects.equals(buttons[posx][posy - 1].getText(), "S"))
+                if (Objects.equals(buttons[posx][posy + 1].getText(), "S"))
+                    score++;
+        }
+        return score;
+    }
+
+    private void setPositions(Button button) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (button == buttons[i][j]) {
                     posx = i;
                     posy = j;
                     break;
@@ -117,17 +128,15 @@ public class GameRules {
         }
     }
 
-    public void endCheck(Player p1, Player p2){
+    public void endCheck(Player p1, Player p2) {
         turn++;
-        if(turn > 36){
+        if (turn > 36) {
             restartButton.setDisable(false);
-            if(p1.getScore() > p2.getScore()){
+            if (p1.getScore() > p2.getScore()) {
                 resultInfo.setText("Wygrał Gracz 1!");
-            }
-            else if(p2.getScore() > p1.getScore()){
+            } else if (p2.getScore() > p1.getScore()) {
                 resultInfo.setText("Wygrał Gracz 2!");
-            }
-            else{
+            } else {
                 resultInfo.setText("Remis!");
             }
         }
